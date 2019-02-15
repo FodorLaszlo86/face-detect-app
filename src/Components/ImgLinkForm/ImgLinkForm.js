579c66ecbfa4cb95e728c402124688a118933832
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-15 11:20:11 +0100

    app components created

---
+++ src/Components/ImgLinkForm/ImgLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImgLinkForm.css';
+
+const ImgLinkForm = () => {
+    return (
+        <div>
+            <p className='center f3'>
+                { 'This magic brain detect faces on images. Just give it a try.' }
+            </p>
+            <div className='form center'>
+                <div className='center pa4 br3 shadow-5'> 
+                    <input type='text' className='f4 pa2 w-70 center' />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImgLinkForm;
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+    return (
+        <div className='center ranking'>
+            <div className='white f3'>
+                {  'Laszlo Your Current Rank is...'}
+            </div>
+            <div className='white f1'>
+                {  '#1'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
