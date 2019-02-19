9d02e5597173536f9ef892b044afe66b97bd87bc
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-19 10:26:41 +0100

    initial server setup

---
+++ smart-brain-api/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+//const bodyParser = require('body-parser');
+
+
+app.listen(3001, () => {
+    console.log('Server is Running on Port: 3001');
+})
