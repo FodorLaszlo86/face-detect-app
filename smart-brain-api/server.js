19257082d488f9190e266f64462889d32d81ec03
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-19 11:45:03 +0100

    profile route skeleton added

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -5,18 +5,21 @@ const bodyParser = require('body-parser');
 
 const dataBase = [
     {
+        id: '1',
         name: 'Kate',
         email: 'kate@gmail.com',
         password: 'puppydog'
     },
 
     {
+        id: '2',
         name: 'Bill',
         email: 'billy@gmail.com',
         password: 't-rex'
     },
 
     {
+        id: '3',
         name: 'Ondria',
         email: 'ondria@gmail.com',
         password: 'shoesandtatoos'
@@ -49,6 +52,20 @@ app.post('/register', (req, res) => {
         joined: new Date()
     })
     res.json(dataBase[dataBase.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    dataBase.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(found === false) {
+        return res.status(400).json('User Not Found')
+    }
 })
 
 app.listen(3001, () => {
